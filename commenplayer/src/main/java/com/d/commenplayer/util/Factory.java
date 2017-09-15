f98193a3502765982940801bef29c4e66a835c02
D <s90789@outlook.com>
2017-09-15 15:44:43 +0800

    添加全屏切换效果：状态栏、导航栏处理

--- commenplayer/src/main/java/com/d/commenplayer/util/Factory.java
+++ commenplayer/src/main/java/com/d/commenplayer/util/Factory.java
@@ -109,7 +109,7 @@ public class Factory {
 <<<<<<< HEAD
 =======
                 //设置rtsp的连接模式为：tcp/udp
-                ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "rtsp_transport", "tcp");
+//                ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "rtsp_transport", "tcp");
 
 >>>>>>> add option
                 ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "http-detect-range-support", 0);
